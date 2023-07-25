49d3abb4778ae627add020497250d89f6d122d12
Philippe Dumonet <philippe@dumo.net>
2023-07-25 14:21:37 +0200

    ⚡ Optimize success check

--- script/deploy/ETHAirdropper.s.sol
+++ script/deploy/ETHAirdropper.s.sol
@@ -8,7 +8,7 @@ import {console2 as console} from "forge-std/console2.sol";
 /// @author philogy <https://github.com/philogy>
 contract ETHAirdropperScript is Script, HuffDeployer {
     function run() public {
-        bytes32[] memory packets = new bytes32[](500);
+        bytes32[] memory packets = new bytes32[](1500);
         uint256 baseAmount = 1 ether;
         for (uint256 i = 0; i < packets.length; i++) {
             packets[i] = bytes32(abi.encodePacked(uint96(baseAmount), vm.addr(i + 3)));
@@ -16,6 +16,10 @@ contract ETHAirdropperScript is Script, HuffDeployer {
         uint256 total = packets.length * baseAmount;
 
         vm.startBroadcast(vm.envUint("PRIV_KEY"));
+
+        for (uint256 i = 0; i < packets.length; i++) {
+            payable(vm.addr(i + 3)).transfer(1 wei);
+        }
 
         address airdropper = deploy("src/ETHAirdropper.huff");
         console.log("size: %s", airdropper.code.length);
